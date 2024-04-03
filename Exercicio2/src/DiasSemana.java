29d88e8f304e1b558b693d2d6299adaeadcd1125
Pedro Henrique <4ndroidplay.games@gmail.com>
2024-04-03 12:01:31 -0300

    Exercicio2 pronto

---
+++ Exercicio2/src/DiasSemana.java
@@ -0,0 +1,13 @@
+public class DiasSemana {
+    public static String dia1 = "Domingo";
+    public static String dia2 = "Segunda";
+    public static String dia3 = "Terça";
+    public static String dia4 = "Quarta";
+    public static String dia5 = "Quinta";
+    public static String dia6 = "Sexta";
+    public static String dia7 = "Sábado";
+
+
+
+
+}
