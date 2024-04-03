859781fc78cd5d2aff964eb5db0f79a0df004c44
Pedro Henrique <4ndroidplay.games@gmail.com>
2024-04-03 09:53:15 -0300

    Exercicio1 feito

--- Exercicio1/src/Main.java
+++ Exercicio1/src/Main.java
@@ -1,5 +1,23 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
+
+        if (numero > 0){
+            JOptionPane.showMessageDialog(null,"Número positivo");
+
+        } else if (numero < 0){
+            JOptionPane.showMessageDialog(null,"Número negativo");
+
+        } else {
+            JOptionPane.showMessageDialog(null,"Número neutro");
+        }
     }
+
+
+
 }
